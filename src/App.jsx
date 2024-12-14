9628d493e743851053b9c78fc388fa98d1955865
SiHyung-Lee <buzzcatfish@gmail.com>
2024-12-14 13:15:51 +0900

    completed task style

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 const App = () => {
   return (
     <div className="container">
-      {/* <Header /> */}
+      <Header />
       <div className="app-box">
         <Outlet />
       </div>
